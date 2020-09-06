da2825a3c43e3829cef0ac1ba71edfe7048a0fa2
Vatsa Prahallada <vatsamail@gmail.com>
2020-09-06 16:03:25 -0700

    created the cool tables

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-
 # Create your views here.
 from django.http import HttpResponse
 
+from datetime import datetime
+
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    now = datetime.now()
+    current_time = now.strftime("%m-%d-%Y, %H:%M:%S")
+    return HttpResponse("Hello, world. The time is: "+str(current_time))
